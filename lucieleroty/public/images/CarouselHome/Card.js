91954952725e49fce7c0b2224b706461a0b5388e
luciel53 <l.leroty@gmail.com>
2024-09-10 17:31:18 +0200

    create the card file + added icons

---
+++ lucieleroty/public/images/CarouselHome/Card.js
@@ -0,0 +1,20 @@
+const HomeCarouselCard = () => {
+	return (
+		<div className="flex flex-row">
+			{/* Description of the card */}
+			<div className="flex flex-col">
+				<div className="flex flex-row">
+					image
+				<h2></h2>
+				</div>
+				<quote></quote>
+				<p></p>
+				<button></button>
+			</div>
+			{/* Picture */}
+			<div></div>
+		</div>
+	);
+};
+
+export default HomeCarouselCard;
