d0fb653777ae83bc5eb89fd29b3d4c0374196bca
luciel53 <l.leroty@gmail.com>
2024-09-10 16:45:36 +0200

    size of the footer text is ok

--- lucieleroty/src/app/components/Footer.js
+++ lucieleroty/src/app/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<footer className=" w-full py-4 bg-lightgray text-center text-black">
+		<footer className=" w-full py-4 text-[0.6rem] md:text-[0.8rem] bg-lightgray text-center text-black">
 			<p>© Lucie Leroty - Développeuse Web Freelance - 2024</p>
 		</footer>
 	)
