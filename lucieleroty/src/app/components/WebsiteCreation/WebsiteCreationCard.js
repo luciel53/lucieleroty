4a5b902cfeb222660fb064575ae8fc3b48102893
luciel53 <l.leroty@gmail.com>
2024-09-16 17:08:37 +0200

    title green in webcreation

--- lucieleroty/src/app/components/WebsiteCreation/WebsiteCreationCard.js
+++ lucieleroty/src/app/components/WebsiteCreation/WebsiteCreationCard.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const WebsiteCreationCard = ({ srcImg, title, ul }) => {
   return (
-    <div className="flex flex-col bg-verylightgray justify-center items-center w-[40%] mb-10 px-5 mx-auto shadow-lg rounded-lg border-2 border-lightgreen animate-fade-up animate-duration-[2000ms] animate-ease-out">
-      <h2 className="p-4 font-specialelite">{title}</h2>
+    <div className="flex flex-col bg-verylightgray justify-center items-center w-[40%] mb-10 px-5 mx-auto shadow-lg rounded-lg border-2 border-darkgreen animate-fade-up animate-duration-[2000ms] animate-ease-out">
+      <h2 className="p-4 font-specialelite text-darkgreen">{title}</h2>
       <div className="flex flex-row pb-4">
         <div>
           <Image src={srcImg} alt={title} width={300} height={300} />
