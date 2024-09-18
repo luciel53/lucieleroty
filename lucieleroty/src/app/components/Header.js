99c3a98032df393f06b4168590eeea65ce75161c
luciel53 <l.leroty@gmail.com>
2024-09-18 19:48:57 +0200

    add hover effect in navbar

--- lucieleroty/src/app/components/Header.js
+++ lucieleroty/src/app/components/Header.js
@@ -8,7 +8,6 @@ import logo from "../../../public/images/logo.png";
 import localFont from "next/font/local";
 import "../globals.css";
 
-
 const glitch = localFont({
   src: "../../../public/fonts/Glitch.ttf",
   variable: "--font-glitch",
@@ -26,18 +25,18 @@ const Header = () => {
     <header className="flex flex-row justify-between w-full lg:justify-around">
       {/* Logo */}
       <Link href="/">
-      <div className="flex flex-row animate-rotate-y animate-once animate-delay-[1000ms] animate-ease-in">
-        <Image
-          src={logo}
-          alt="Logo"
-          className="w-10 h-10 md:w-16 md:h-16 lg:w-16 lg:h-16 drop-shadow-left"
-        />
-        <span
-          className={`font-glitch-important text-xs md:text-lg mt-3 md:mt-4 -ml-1 md:-ml-2 ${glitch.variable} antialiased`}
-        >
-          EYE<span className="text-darkgreen">TO</span>PIXEL
-        </span>
-      </div>
+        <div className="flex flex-row animate-rotate-y animate-once animate-delay-[1000ms] animate-ease-in">
+          <Image
+            src={logo}
+            alt="Logo"
+            className="w-10 h-10 md:w-16 md:h-16 lg:w-16 lg:h-16 drop-shadow-left"
+          />
+          <span
+            className={`font-glitch-important text-xs md:text-lg mt-3 md:mt-4 -ml-1 md:-ml-2 ${glitch.variable} antialiased`}
+          >
+            EYE<span className="text-darkgreen">TO</span>PIXEL
+          </span>
+        </div>
       </Link>
       {/* Container to hide overflow */}
       <div className="relative overflow-hidden">
@@ -64,34 +63,54 @@ const Header = () => {
           className={`lg:z-30 lg:flex flex-row lg:space-x-10 text-xs md:text-base lg:text-lg lg:mr-4 lg:mt-1`}
         >
           <li>
-          <Link href="/">
-            Accueil
+            <Link href="/">
+              <p class="text-lg group relative w-max">
+                <span>Accueil</span>
+                <span class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-darkgreen group-hover:w-3/6"></span>
+                <span class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-darkgreen group-hover:w-3/6"></span>
+              </p>
             </Link>
-            </li>
+          </li>
           <li>
             <Link href="/website-creation">
-              Création de sites
+              <p class="text-lg group relative w-max">
+                <span>Création de sites</span>
+                <span class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-darkgreen group-hover:w-3/6"></span>
+                <span class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-darkgreen group-hover:w-3/6"></span>
+              </p>
             </Link>
           </li>
           <li>
             <Link href="/design">
-            Design
+              <p class="text-lg group relative w-max">
+                <span>Design</span>
+                <span class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-darkgreen group-hover:w-3/6"></span>
+                <span class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-darkgreen group-hover:w-3/6"></span>
+              </p>
             </Link>
-            </li>
+          </li>
           <li>
             <Link href="/photography">
-            Photographie
+              <p class="text-lg group relative w-max">
+                <span>Photographie</span>
+                <span class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-darkgreen group-hover:w-3/6"></span>
+                <span class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-darkgreen group-hover:w-3/6"></span>
+              </p>
             </Link>
-            </li>
+          </li>
           <li className="lg:text-white">
             <Link href="/contact">
-            Contact
-            </Link>
-            </li>
+            <p class="text-lg group relative w-max">
+                <span>Contact</span>
+                <span class="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-verylightgray group-hover:w-3/6"></span>
+                <span class="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-verylightgray group-hover:w-3/6"></span>
+              </p>
+              </Link>
+          </li>
         </ul>
         {/* Bubble on Contact */}
         <div className="relative">
-        <span className="lg:z-10 flex absolute drop-shadow-lg animate-bounce animate-duration-[800ms] animate-delay-[20000ms] animate-ease-out lg:w-40 lg:h-40 lg:bg-darkgreen text-white rounded-full lg:-mt-20 lg:-right-12 overflow-hidden"></span>
+          <span className="lg:z-10 flex absolute drop-shadow-lg animate-bounce animate-duration-[800ms] animate-delay-[20000ms] animate-ease-out hover:animate-none lg:w-40 lg:h-40 lg:bg-darkgreen text-white rounded-full lg:-mt-20 lg:-right-12 overflow-hidden"></span>
         </div>
       </nav>
     </header>
