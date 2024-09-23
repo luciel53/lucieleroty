4f17b9354b8b384dc83b9a4fd6f258a19fc4a678
luciel53 <l.leroty@gmail.com>
2024-09-23 23:28:20 +0200

    created  legal notices

---
+++ lucieleroty/src/app/legalNotices/page.js
@@ -0,0 +1,39 @@
+const legalNotices = () => {
+  return (
+    <main>
+      <h1>Mentions Légales</h1>
+      <div>
+        <h2>L'entreprise:</h2>
+        <p>EyeToPixel</p>
+        <p>7 rue Edouard de la boussinière</p>
+        <p>Le Mans</p>
+        <p>07.78.95.65.12</p>
+        <p>contact@eyetopixel.com</p>
+        <p>818218117</p>
+      </div>
+
+      <div>
+        <h2>L'hébergeur:</h2>
+        <p>Hostinger International Ltd.</p>
+        <p>61 Lordou Vironos Street, 6023</p>
+        <p>Larnaca, Cyprus</p>
+        <p>https://www.hostinger.com/contacts</p>
+      </div>
+
+      <div>
+        <h2>Directeur de la publication:</h2>
+        <p>Lucie Leroty, dirigeant de EyeToPixel</p>
+      </div>
+
+      <div>
+        <h2>Création du site:</h2>
+        <p>EyeToPixel</p>
+        <p>Création de sites web et Photographie</p>
+        <p>www.eyetopixel.com</p>
+        Cookies:
+      </div>
+    </main>
+  );
+};
+
+export default legalNotices();
