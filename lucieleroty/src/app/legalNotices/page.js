8cc508c11bd94c754cc186187c9f37eb48198992
luciel53 <l.leroty@gmail.com>
2024-09-25 16:23:00 +0200

    legalnotices page is responsive

--- lucieleroty/src/app/legalNotices/page.js
+++ lucieleroty/src/app/legalNotices/page.js
@@ -1,13 +1,13 @@
 const LegalNotices = () => {
   return (
     <main className="flex flex-col">
-      <h1 className="font-specialelite text-xl mt-5 text-center">
+      <h1 className="font-specialelite text-sm md:text-xl lg:text-xl mt-5 text-center">
         Mentions Légales
       </h1>
 
       {/* Company informations */}
-      <div className="ml-40 mt-16">
-        <h2 className="mb-2 underline font-specialelite text-base">
+      <div className=" mx-4 md:mx-10 lg:ml-40 mt-5 md:mt-16 text-sm md:text-base">
+        <h2 className="mb-2 underline font-specialelite text-xs md:text-base">
           L'entreprise :
         </h2>
         <p>
@@ -36,14 +36,14 @@ const LegalNotices = () => {
         </p>
         <p>n° de SIREN : 818218117</p>
         <p>
-          Directrice et créatrice de la publication : Lucie Leroty, dirigeante
+          Directrice et créatrice de la publication: Lucie Leroty, dirigeante
           de EyeToPixel
         </p>
       </div>
 
       {/* Hosting company */}
-      <div className="ml-40 mt-6">
-        <h2 className="mb-2 underline font-specialelite text-base">
+      <div className=" mx-4 md:mx-10 lg:ml-40 mt-5 md:mt-16 text-sm md:text-base">
+        <h2 className="mb-2 underline font-specialelite text-xs md:text-base">
           L'hébergeur :
         </h2>
         <p>Hostinger International Ltd.</p>
@@ -62,16 +62,16 @@ const LegalNotices = () => {
       </div>
 
       {/* Data privacy */}
-      <div className="ml-40 mt-6 mb-10">
-        <h2 className="mb-2 underline font-specialelite text-base">
+      <div className=" mx-4 md:mx-10 lg:mx-40 mt-5 md:mt-16 mb-10 text-sm md:text-base">
+        <h2 className="mb-2 underline font-specialelite text-xs md:text-base">
           Protection des données :
         </h2>
-        <p className=" w-[83%] text-justify">
+        <p className="text-justify">
           Vos données personnelles sont traitées avec la plus stricte
           confidentialité. Nous ne collectons que les informations nécessaires
           afin de vous contacter via notre formulaire de contact.
         </p>
-        <p className=" w-[83%] text-justify">
+        <p className="text-justify">
           Vous avez le droit d'accéder à vos données, de les modifier ou de
           demander leur suppression à tout moment. Pour exercer ces droits,
           veuillez nous contacter à l'adresse suivante:{" "}
